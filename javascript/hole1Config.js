09e946577f9b817c75c0aae24a402871e35351d5
muguira-james <muguira.james@gmail.com>
2017-06-28 18:03:00 -0400

    next version 1

---
+++ javascript/hole1Config.js
@@ -0,0 +1,120 @@
+var hole1Config = {
+  "type": "FeatureCollection",
+  "Features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hole_1",
+        "Flag_number": 1
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [40.655093, -74.692860]
+      }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Tee Location",
+      "photo": "./images/Tee2.png"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.653678, -74.692720]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Flag",
+      "Flag_number": 1
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.656333, -74.693523]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "lineCoordinates"
+    },
+    "geometry": {
+      "type": "LineString",
+      "coordinates": [
+          [-74.692720, 40.653678],
+          [-74.692860, 40.655093],
+          [-74.693523, 40.656333]
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "TeeBoxLeft",
+      "notes": "teebox: lef, right; fairway: left, right; green: left, right"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65366,  -74.69295]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "TeeBoxRight",
+      "notes": "teebox: lef, right; fairway: left, right; green: left, right"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65366, -74.69277]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "FairwayLeft",
+
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65505,  -74.69308]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "FairwayRight",
+      "notes": "teebox: lef, right; fairway: left, right; green: left, right"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65497,  -74.69274]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "GreenLeft",
+      "notes": "teebox: lef, right; fairway: left, right; green: left, right"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65631,  -74.69374]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "GreenLeft",
+      "notes": "teebox: lef, right; fairway: left, right; green: left, right"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [40.65636,  -74.69338]
+    }
+  }
+
+  ]
+
+}
---
+++ javascript/playerdb.js
@@ -0,0 +1,85 @@
+var playerdb = {
+  "type": "FeatureCollection",
+  "Features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Holly Clyburn",
+        "photo": "./images/2016_clyburn_h---full_470x486.jpg",
+        "currentHole": 1,
+        "locationOnHole": "tee"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [0,0]
+      }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Brooke Pancake",
+      "photo": "./images/pancake_b466607988_620x650px.png",
+      "currentHole": 1,
+      "locationOnHole": "tee"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [0,0]
+    }
+  },
+  {
+  "type": "Feature",
+  "properties": {
+    "name": "Cheyenne Woods",
+    "photo": "./images/cheyenne-woods_620x650_bio.png",
+    "currentHole": 1,
+    "locationOnHole": "fairway"
+  },
+  "geometry": {
+    "type": "Point",
+    "coordinates": [0,0]
+  }
+},
+{
+"type": "Feature",
+"properties": {
+  "name": "Katie Burnett",
+  "photo": "./images/burnett-katie-hs-276x273.png",
+  "currentHole": 1,
+  "locationOnHole": "fairway"
+},
+"geometry": {
+  "type": "Point",
+  "coordinates": [0,0]
+}
+},
+{
+"type": "Feature",
+"properties": {
+  "name": "Perrine Delacour",
+  "photo": "./images/delacour_p_466624800_2015_620x650.png",
+  "currentHole": 1,
+  "locationOnHole": "green"
+},
+"geometry": {
+  "type": "Point",
+  "coordinates": [0,0]
+}
+},
+{
+"type": "Feature",
+"properties": {
+  "name": "Ayako Uehara",
+  "photo": "./images/uehara-ayako-hs-276x273.png",
+  "currentHole": 1,
+  "locationOnHole": "green"
+},
+"geometry": {
+  "type": "Point",
+  "coordinates": [0,0]
+}
+},
+
+  ]
+
+}
