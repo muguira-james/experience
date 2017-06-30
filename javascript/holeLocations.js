96c079c996541acaed13f591feb44efb08f87f03
muguira-james <muguira.james@gmail.com>
2017-06-30 16:09:12 -0400

    check the course view

---
+++ javascript/holeLocations.js
@@ -0,0 +1,149 @@
+var holeLatLong = {
+  "type": "FeatureCollection",
+  "Features": [
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_1.png",
+            "Flag_number": 1
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.655093, -74.692860]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_2.png",
+            "Flag_number": 2
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.65735, -74.69293]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_3.png",
+            "Flag_number": 3
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.65486, -74.69137]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_4.png",
+            "Flag_number": 4
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64971, -74.69141]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_5.png",
+            "Flag_number": 5
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64652, -74.69218]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_6.png",
+            "Flag_number": 6
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64514, -74.6958]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_7.png",
+            "Flag_number": 7
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64454, -74.69785]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_8.png",
+            "Flag_number": 8
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64592, -74.69703]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_9.png",
+            "Flag_number": 9
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.64748, -74.69629]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_10.png",
+            "Flag_number": 10
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.65181, -74.69986]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_11.png",
+            "Flag_number": 11
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.65554, -74.70227]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_12.png",
+            "Flag_number": 12
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.65966, -74.70492]
+          }
+      },
+      {
+          "type": "Feature",
+          "properties": {
+            "name": "number_13.png",
+            "Flag_number": 13
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [40.66055, -74.70604]
+          }
+      },
+
+  ]
+};
