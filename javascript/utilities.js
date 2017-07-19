1e3918f42950f4da76ba56ba70d7565af587b11a
muguira-james <muguira.james@gmail.com>
2017-07-19 14:02:12 -0400

    newer

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -138,7 +138,7 @@ var drawCourse = function() {
     var HoleIcon = L.icon ({
         iconUrl: icoName,
         iconAnchor: [8,35],
-        iconSize: [80,80]
+        iconSize: [100,100]
       });
 
     marker = L.marker(element.geometry.coordinates, { icon: HoleIcon}).addTo(myCourseView);
