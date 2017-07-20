025f92ade1a3ced6dd40f2d1135889a779766a3b
muguira-james <muguira.james@gmail.com>
2017-07-20 12:48:49 -0400

    newer

--- javascript/playerdb_1.js
+++ javascript/playerdb_1.js
@@ -67,9 +67,146 @@ var playerdb = {
       "position": [42.0, -74.0]
     }
     },
-
-
-
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player01",
+      "photo": "./images/Player01.png",
+      "currentHole": 9,
+      "locationOnHole": "fairway",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player02",
+      "photo": "./images/Player02.png",
+      "currentHole": 9,
+      "locationOnHole": "tee",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player03",
+      "photo": "./images/Player03.png",
+      "currentHole": 15,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player04",
+      "photo": "./images/Player04.png",
+      "currentHole": 15,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player05",
+      "photo": "./images/Player05.png",
+      "currentHole": 5,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player06",
+      "photo": "./images/Player06.png",
+      "currentHole": 5,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player07",
+      "photo": "./images/Player07.png",
+      "currentHole": 3,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player08",
+      "photo": "./images/Player08.png",
+      "currentHole": 3,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player09",
+      "photo": "./images/Player09.png",
+      "currentHole": 16,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player10",
+      "photo": "./images/Player10.png",
+      "currentHole": 16,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player11",
+      "photo": "./images/Player11.png",
+      "currentHole": 17,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player12",
+      "photo": "./images/Player12.png",
+      "currentHole": 17,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player13",
+      "photo": "./images/Player13.png",
+      "currentHole": 8,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
+    {
+    "type": "Feature",
+    "properties": {
+      "name": "Player14",
+      "photo": "./images/Player14.png",
+      "currentHole": 8,
+      "locationOnHole": "green",
+      "position": [42.0, -74.0]
+    }
+    },
   ]
 
 }
